1309a6f55c20e56942dcb05b1fbe1155689638e0
Arran Bartish <arranbartish@hotmail.com>
2016-04-16 18:21:29 -0400

    Ensure tests fail and run on correct phase

--- jar-dashboard/src/test/java/com/solvedbysunrise/appdirect/AppDirectDashboardApplicationTest.java
+++ jar-dashboard/src/test/java/com/solvedbysunrise/appdirect/AppDirectDashboardApplicationTest.java
@@ -1,5 +1,6 @@
 package com.solvedbysunrise.appdirect;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -7,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AppDirectDashboardApplication.class)
-public class AppDirectDashboardApplicationTests {
+public class AppDirectDashboardApplicationTest {
 
 	@Test
 	public void contextLoads() {
--- jar-dashboard/src/test/java/com/solvedbysunrise/appdirect/HelloWorldControllerIntegrationTest.java
+++ jar-dashboard/src/test/java/com/solvedbysunrise/appdirect/HelloWorldControllerIntegrationTest.java
@@ -3,11 +3,12 @@ package com.solvedbysunrise.appdirect;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @WebIntegrationTest
-public class HelloWorldControllerTest {
+public class HelloWorldControllerIntegrationTest {
 
     @Test
     public void serviceWillReturnWorld() throws Exception {
