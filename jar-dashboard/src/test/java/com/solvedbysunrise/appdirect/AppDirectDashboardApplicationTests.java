e915e66dd38309379457310abfbe964d9f47ae88
Arran Bartish <arranbartish@hotmail.com>
2016-04-16 16:02:12 -0400

    basic spring boot start of a hello world to test ci and deployment

---
+++ jar-dashboard/src/test/java/com/solvedbysunrise/appdirect/AppDirectDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solvedbysunrise.appdirect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AppDirectDashboardApplication.class)
+public class AppDirectDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ jar-dashboard/src/test/java/com/solvedbysunrise/appdirect/HelloWorldControllerTest.java
@@ -0,0 +1,16 @@
+package com.solvedbysunrise.appdirect;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@WebIntegrationTest
+public class HelloWorldControllerTest {
+
+    @Test
+    public void serviceWillReturnWorld() throws Exception {
+        Assert.fail("implement");
+    }
+}
