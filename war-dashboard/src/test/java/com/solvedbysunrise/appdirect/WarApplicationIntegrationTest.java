c720d212e0d86eb701d664a569bf7ba3b6d8c7e5
Arran Bartish <arranbartish@hotmail.com>
2016-04-17 19:02:09 -0400

    update packaging to include a war and validate it is deployable

---
+++ war-dashboard/src/test/java/com/solvedbysunrise/appdirect/WarApplicationIntegrationTest.java
@@ -0,0 +1,43 @@
+package com.solvedbysunrise.appdirect;
+
+import com.solvedbysunrise.appdirect.dto.Hello;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriTemplate;
+
+import static com.solvedbysunrise.appdirect.util.UriUtil.getFullyQualifiedUriPattern;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+@Rollback
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {WarApplication.class, IntegrationTestConfiguration.class})
+public class WarApplicationIntegrationTest {
+
+    @Autowired
+    private String dashboardBaseUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final Hello EXPECTED_WORLD = new Hello();
+    {
+        EXPECTED_WORLD.setWho("world");
+    }
+
+    @Test
+    public void application_will_start() {
+        UriTemplate template = new UriTemplate(getFullyQualifiedUriPattern(dashboardBaseUrl, "/hello"));
+        Hello world =  restTemplate.getForEntity(template.expand(), Hello.class).getBody();
+        Assert.assertThat(world, is(EXPECTED_WORLD));
+    }
+}
