572ec9497f5827126969ba1f7c893aa2f8bc9293
Arran Bartish <arranbartish@hotmail.com>
2016-04-24 17:28:03 -0400

    Angular JS integrated for frontend.
    issue: War distribution does not make static resources available.

---
+++ jar-dashboard/src/main/resources/static/script.js
@@ -0,0 +1,39 @@
+angular.module("DashApp", ["ngAnimate","ui.bootstrap"])
+    .config(["$httpProvider", function($httpProvider){
+
+    }])
+    .service("PurchaseService", function(){
+
+            this.populatePurchases= function(scope, http) {
+
+                scope.purchases = [{ name : "mock1" }, { name : "mock1" }];
+                // http.get('/purchases')
+                //   //  http.get('/status')
+                //     .success(function(response){
+                //         scope.purchases = response;
+                //     });
+            };
+    })
+    .controller("AppPurchases", function($scope, $http, $timeout, $log, PurchaseService){
+        PurchaseService.populatePurchases($scope, $http);
+
+        var poll = function () {
+            $timeout(function(){
+                PurchaseService.populatePurchases($scope, $http);
+                poll();
+            }, 5000);
+            //}, 12000000);
+        };
+        poll();
+    })
+    .directive('purchases', function() {
+        return {
+            restrict: 'E',
+            replace: 'false',
+            scope: {
+                purchase: '='
+            },
+            templateUrl: './templates/purchase.html'
+        }
+    });
+
